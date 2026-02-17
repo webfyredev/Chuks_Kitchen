1aea7414ae55ef63686ebe61484aa73ed6b1dd0d
impphilwebs <imphilwebs@gmail.com>
2026-02-17 10:03:08 +0100

    Added more functionality to the explore page

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -6,6 +6,7 @@ import { jollof_rice, popular_cats, soups } from "../utils/explore";
 import { FaPlus } from "react-icons/fa";
 import Footer from "../components/footer";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Explore(){
     useEffect(() => {
@@ -48,28 +49,34 @@ export default function Explore(){
                         Popular
                     </h3>
                     <div className="w-full py-5 mt-3 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                        {popular_cats.map((data, index) => (
-                            <motion.div 
-                                {...scrollRightEffects}
-                                key={index} className="flex flex-row space-x-3 md:space-x-0 items-center bg-white  md:flex md:flex-col rounded-xl  md:mb-5 group overflow-hidden hover:shadow-sm hover:transition-all hover:duration-300 px-2 md:px-0">
-                                <img src={data.image} alt={data.title} className="rounded-sm md:rounded-t-xl w-[30%] md:w-full h-25 md:h-50 object-cover group-hover:scale-105 transition-all duration-300 cursor-pointer" />
-                                <div className="flex flex-col items-left justify-center px-2 md:py-5 bg-white md:rounded-b-xl h-35 md:h-40 w-[70%] md:w-full">
-                                    <h3 className="text-[#1F2937] font-semibold text-[15px] group-hover:text-[#FF7A18] mb-2">
-                                        {data.title}
-                                    </h3>
-                                    <p className="text-[13px] text-[#1F2937]">
-                                        {data.text}
-                                    </p>
-                                    <div className="flex items-center justify-between mt-3 md:mt-5">
-                                        <p className="text-[#FF7A18] font-semibold text-[13px]">
-                                            #{data.price}
+                        {popular_cats.map((data, index) => {
+                            const hideOnMobile = index < 3;
+                            return(
+                                <motion.div 
+                                    {...scrollRightEffects}
+                                    key={index} className={`${hideOnMobile ? "hidden md:flex" : "flex"} flex flex-row space-x-3 md:space-x-0 items-center bg-white  md:flex md:flex-col rounded-xl  md:mb-5 group overflow-hidden hover:shadow-sm hover:transition-all hover:duration-300 px-2 md:px-0`}>
+                                    <img src={data.image} alt={data.title} className="rounded-sm md:rounded-t-xl w-[30%] md:w-full h-25 md:h-50 object-cover group-hover:scale-105 transition-all duration-300 cursor-pointer" />
+                                    <div className="flex flex-col items-left justify-center px-2 md:py-5 bg-white md:rounded-b-xl h-35 md:h-40 w-[70%] md:w-full">
+                                        <h3 className="text-[#1F2937] font-semibold text-[15px] group-hover:text-[#FF7A18] mb-2">
+                                            {data.title}
+                                        </h3>
+                                        <p className="text-[13px] text-[#1F2937]">
+                                            {data.text}
                                         </p>
-                                        <FaPlus  className="mr-3 border w-6 h-6 p-1.5 rounded-full cursor-pointer bg-[#FF7A18] text-white"/>
+                                        <div className="flex items-center justify-between mt-3 md:mt-5">
+                                            <p className="text-[#FF7A18] font-semibold text-[13px]">
+                                                #{data.price}
+                                            </p>
+                                            <FaPlus  className="mr-3 border w-6 h-6 p-1.5 rounded-full cursor-pointer bg-[#FF7A18] text-white"/>
+                                        </div>
                                     </div>
-                                </div>
-                            </motion.div>
-                        ))}
+                                </motion.div>
+                            )
+                        })}
                     </div>
+                    <Link  to="/" className="mt-2 w-full flex lg:hidden items-center justify-center text-[#1E88E5] font-semibold text-[13px] ">
+                        View Categories
+                    </Link>
                 </div>
                 <div className="w-full md:px-8 py-5 flex flex-col">
                     <h3 className="font-bold text-[#1F2937] text-xl">
@@ -98,6 +105,9 @@ export default function Explore(){
                             </motion.div>
                         ))}
                     </div>
+                    <Link  to="/" className="mt-2 w-full flex lg:hidden items-center justify-center text-[#1E88E5] font-semibold text-[13px] ">
+                        View Categories
+                    </Link>
                 </div>
                 <div className="w-full md:px-8 py-5 flex flex-col">
                     <h3 className="font-bold text-[#1F2937] text-xl">
@@ -126,6 +136,9 @@ export default function Explore(){
                             </motion.div>
                         ))}
                     </div>
+                    <Link  to="/" className="mt-2 w-full flex lg:hidden items-center justify-center text-[#1E88E5] font-semibold text-[13px] ">
+                        View Categories
+                    </Link>
                 </div>
         </div>
         <div className="w-full p-10 bg-white">
